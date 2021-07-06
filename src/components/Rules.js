602905b72716e819f0b0c2502882bf8e285665e4
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-07-06 17:56:37 -0400

    updating rules

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './rules.css';
-import rulesImage from '../images/image-rules.svg';
+import rulesImage from '../images/image-rules-bonus.svg';
 
 export default function Rules(props) {
     return (
