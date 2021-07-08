c0615347b468f436d466bf2165a8785a9ee92d78
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-07-08 18:35:39 -0400

    rock paper scissors lizard spock

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -7,7 +7,7 @@ export default function Winner(props) {
         <div className="containerDesktop">
             <div className="youPickedDiv">
                 <h2 className="youPickedTitle">YOU PICKED</h2>
-                <div className={props.playersChoice === "rock" ? "rockButtonBorderWinner" : props.playersChoice === "paper" ? "paperButtonBorderWinner" : "scissorButtonBorderWinner"} onClick={null}><button className={props.playersChoice === "rock" ? "rockButtonWinner" : props.playersChoice === "paper" ? "paperButtonWinner" : "scissorButtonWinner"}></button></div>
+                <div className={props.playersChoice === "rock" ? "rockButtonBorderWinner" : props.playersChoice === "paper" ? "paperButtonBorderWinner" : props.playersChoice === "scissors" ? "scissorButtonBorderWinner" : props.playersChoice === "lizard" ? "lizardButtonBorderWinner" : "spockButtonBorderWinner"} onClick={null}><button className={props.playersChoice === "rock" ? "rockButtonWinner" : props.playersChoice === "paper" ? "paperButtonWinner" : props.playersChoice === "scissors" ? "scissorButtonWinner" : props.playersChoice === "lizard" ? "lizardButtonWinner" : "spockButtonWinner"}></button></div>
             </div>
             <div className="winnerIsDiv">
                 <h1 className="winnerIsTitle">{props.playerIsWinner === true ? "YOU WIN" : props.computerIsWinner === true ? "YOU LOSE" : "TIE"}</h1>
@@ -15,17 +15,17 @@ export default function Winner(props) {
             </div>
             <div className="theHousePickedDiv">
                 <h2 className="theHousePickedTitle">THE HOUSE PICKED</h2>
-                <div className={props.computersChoice === "rock" ? "rockButtonBorderWinner" : props.computersChoice === "paper" ? "paperButtonBorderWinner" : "scissorButtonBorderWinner"} onClick={null}><button className={props.computersChoice === "rock" ? "rockButtonWinner" : props.computersChoice === "paper" ? "paperButtonWinner" : "scissorButtonWinner"}></button></div>
+                <div className={props.computersChoice === "rock" ? "rockButtonBorderWinner" : props.computersChoice === "paper" ? "paperButtonBorderWinner" : props.computersChoice === "scissors" ? "scissorButtonBorderWinner" : props.computersChoice === "lizard" ? "lizardButtonBorderWinner" : "spockButtonBorderWinner"} onClick={null}><button className={props.computersChoice === "rock" ? "rockButtonWinner" : props.computersChoice === "paper" ? "paperButtonWinner" : props.computersChoice === "scissors" ? "scissorButtonWinner" : props.computersChoice === "lizard" ? "lizardButtonWinner" : "spockButtonWinner"}></button></div>
             </div>
         </div>
         <div className="containerMobile">
             <div className="youPickedDiv">
                 <h2 className="youPickedTitle">YOU PICKED</h2>
-                <div className={props.playersChoice === "rock" ? "rockButtonBorderWinner" : props.playersChoice === "paper" ? "paperButtonBorderWinner" : "scissorButtonBorderWinner"} onClick={null}><button className={props.playersChoice === "rock" ? "rockButtonWinner" : props.playersChoice === "paper" ? "paperButtonWinner" : "scissorButtonWinner"}></button></div>
+                <div className={props.playersChoice === "rock" ? "rockButtonBorderWinner" : props.playersChoice === "paper" ? "paperButtonBorderWinner" : props.playersChoice === "scissors" ? "scissorButtonBorderWinner" : props.playersChoice === "lizard" ? "lizardButtonBorderWinner" : "spockButtonBorderWinner"} onClick={null}><button className={props.playersChoice === "rock" ? "rockButtonWinner" : props.playersChoice === "paper" ? "paperButtonWinner" : props.playersChoice === "scissors" ? "scissorButtonWinner" : props.playersChoice === "lizard" ? "lizardButtonWinner" : "spockButtonWinner"}></button></div>
             </div>
             <div className="theHousePickedDiv">
                 <h2 className="theHousePickedTitle">THE HOUSE PICKED</h2>
-                <div className={props.computersChoice === "rock" ? "rockButtonBorderWinner" : props.computersChoice === "paper" ? "paperButtonBorderWinner" : "scissorButtonBorderWinner"} onClick={null}><button className={props.computersChoice === "rock" ? "rockButtonWinner" : props.computersChoice === "paper" ? "paperButtonWinner" : "scissorButtonWinner"}></button></div>
+                <div className={props.computersChoice === "rock" ? "rockButtonBorderWinner" : props.computersChoice === "paper" ? "paperButtonBorderWinner" : props.computersChoice === "scissors" ? "scissorButtonBorderWinner" : props.computersChoice === "lizard" ? "lizardButtonBorderWinner" : "spockButtonBorderWinner"} onClick={null}><button className={props.computersChoice === "rock" ? "rockButtonWinner" : props.computersChoice === "paper" ? "paperButtonWinner" : props.computersChoice === "scissors" ? "scissorButtonWinner" : props.computersChoice === "lizard" ? "lizardButtonWinner" : "spockButtonWinner"}></button></div>
             </div>
         </div>
         <div className="winnerIsDivMobile">
